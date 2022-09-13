da564ca7cd848a25ed6b9198c4d04a12b86e34dc
lariciamelo <lariciamelo@gmail.com>
2022-09-13 18:52:20 -0300

    Classone

---
+++ src/main/java/Aluno/CadAluno.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aluno;
+/**
+ *
+ * @author Larícia
+ */
+public class CadAluno {
+    
+}
---
+++ src/main/java/TelasGraficas/Principal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TelasGraficas;
+
+import Disciplinas.CadDisciplinas;
+
+/**
+ *
+ * @author Larícia
+ */
+public class Principal {
+    Disciplinas.CadDisciplinas aa = new CadDisciplinas();
+   
+    
+}
